046792a5e72043a0f3d67b37991f23d2b06830a0
Saurabh Srivastava <saurabhs@evertz.com>
2024-07-19 07:25:42 +0530

    removed node modules

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json('MY SERVER IS RUNNING!!!'))
+app.get('/', (req, res) => {
+    console.log("whact")
+    return res.json('MY SERVER IS RUNNING!!!');
+})
 
 app.use("/payment", require("./routes/payment"));
 
