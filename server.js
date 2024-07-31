2674156f214feaa7cc0ae6214b78bf06c082a994
Saurabh Srivastava <saurabhs@evertz.com>
2024-07-31 18:18:56 +0530

    cors change

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ var cors = require('cors');
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.json({ extended: false }));
 
